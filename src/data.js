7c49645b9a8934a16b3dda65037a943b133cdadd
Judith <baruki110214@gmail.com>
2018-07-11 06:59:46 -0500

    cambios entrega 11Julio

--- src/data.js
+++ src/data.js
@@ -1,10 +1,34 @@
-//const sedes = document.getElementById('sedes')
-window.computeStudentsStats = (laboratoria) => {
+//let btngeneracion = document.getElementsByClassName("seleccion1");
+
+window.sortStudents=()=>{}
+
+window.filterStudents=()=>{}
+
+window.computeStudentsStats=(laboratoria) =>{
+
+  const st = {
+    name :"",
+    email : "",
+    campus : "",
+    generation: "",
+    stats :{
+      status :"",
+      completedPercentage :"",
+      topics :{
+        completedPercentage :"",
+        percentageDuration : 0,
+        subtopics : {
+            completedPercentage:0 ,
+          type:0,
+            duration:0
+        }
+      }
+    }
+  }
 }
 
 window.computeGenerationsStats = (laboratoria) => {
 
-  console.log(laboratoria);
   const generationsArray =[];
 
   const obj = {
@@ -17,17 +41,20 @@ window.computeGenerationsStats = (laboratoria) => {
   //console.log(obj);
   let average =0;
 
-  for(key in laboratoria){
+  for(key in laboratoria) {
 
     obj.campus = key;
-    console.log(obj.campus);
+    //console.log(obj.campus);
 
     average = 0;
     const generations = Object.keys(laboratoria[key].generacion);
-    //console.log(laboratoria[key].generacion);
+    console.log(generations);
+
+
+
       generations.forEach((generation) => {
         obj.generation = generation;
-        console.log(obj.generation);
+       // console.log(obj.generation);
         const students = laboratoria[key].generacion[generation].estudiantes;
       //console.log(students);
 
@@ -36,24 +63,24 @@ window.computeGenerationsStats = (laboratoria) => {
           average += array[i].progreso.porcentajeCompletado;
           average = average / array.lenght;
             obj.campus= key;
-            obj.generation= generation;
+        obj.generation= generation;
             obj.average = average;
             obj.count= array.length;
-            console.log(student);
+            //console.log(student);
+            generationsArray.push(student);
         })
 
 
-        /*for (student in students) {
+      /* for (student in students) {
           average += students[student].progreso.porcentajeCompletado;
           average = average / students.lenght;
-
           obj.average = average;
           obj.count = students.lenght;
           console.log(obj.average);
           console.log(obj.count);
         }*/
       //  console.log(generationsArray);
-//console.log(generationsArray.push(obj));
+// console.log(generationsArray.push(obj));
       })
 
 //console.log(generationsArray.push(obj));
@@ -62,3 +89,4 @@ window.computeGenerationsStats = (laboratoria) => {
 //console.log(generationsArray);
 return generationsArray;
 }
+//console.log(generationsArray);
